7ff4c78e8c58c2ee473dd37817a79483ea27323f
Victoria <victori.antonovav@gmail.com>
2018-11-07 14:16:46 +0300

    /create

--- index.js
+++ index.js
@@ -31,6 +31,37 @@ app.post('/read', (req, res) => {
     res.send(films[films.findIndex(film => film.id === req.body.id)]);
 });
 
+function ValidCreate(req) {
+    if (req.body.title !== undefined && req.body.rating !== undefined && req.body.rating > 0 && req.body.year !== undefined
+        && req.body.budget !== undefined && req.body.gross !== undefined && req.body.poster !== undefined && req.body.position !== undefined
+        && req.body.year > 0 && req.body.budget > 0 && req.body.gross > 0 && req.body.position > 0)
+        return true;
+    else
+        return false;
+}
+
+
+app.post('/create', (req, res) => {
+    if (ValidCreate(req)) {
+        req.body.id = films.length;
+        for (let i = 0; i < films.length; i++) {
+            if (req.body.position === films[i].position) {
+                for (let j = i; j < films.length; j++) {
+                    films[j].position++;
+                }
+            }
+        }
+        films.push(req.body);
+        fs.writeFile("top250.json", JSON.stringify(films), "utf8", function () {});
+        RefreshPosition();
+        res.send(All());
+    }
+    else {
+        res.send("{code: 400, message: Request invalid}");
+    }
+});
+
+
 //
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
