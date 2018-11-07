da8f3fea0fa75dac8dab477ae04150262c3a49c3
Victoria <victori.antonovav@gmail.com>
2018-11-07 14:07:46 +0300

    /readall

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
+let films = require("./top250.json");
+const fs = require('fs');
+let bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+function RefreshPosition() {
+    films.sort((a, b) => {
+        return a.position - b.position;
+    });
+    let a = 1;
+    for (let i = 0; i < films.length; i++) {
+        films[i].position = a;
+        ++a;
+    }
+}
+
+function All() {
+    RefreshPosition();
+    return films;
+}
+
+app.get('/readAll', (req, res) => {
+    res.send(All());
 });
+
+//
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-})
+});
